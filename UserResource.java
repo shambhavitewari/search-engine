c08daa926e30d7e0d81d178669f7208b846f9b91
Shambhavi Tewari <33371065+shambhavitewari@users.noreply.github.com>
2019-08-24 22:53:40 +0530

    Add files via upload

--- UserResource.java
+++ UserResource.java
@@ -2,23 +2,25 @@ package com.springboot.project.searchengine;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 public class UserResource {
 
-@Autowired
- private UserRepo repo;
+    @Autowired
+    private UserRepo repo;
 
     @GetMapping("/jpa/users")
-    public List<User> retrieveAllUsers(){
+    public List<User> retrieveAllUsers() {
         return repo.findAll();
 
     }
 
-   /* @GetMapping("/jpa/fname/users")
-    public void retrievefromFname(){
-        System.out.println( repo.findbyFname("Diksha"));*/
+    @GetMapping("/jpa/{fname}/users")
+    public List<User> retrievefromFname(@PathVariable String fname) {
+       return repo.findByfname(fname);
 
+    }
 }

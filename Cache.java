58e55bc280aea45e893ab9e38f8bdd984cc40980
Shambhavi Tewari <33371065+shambhavitewari@users.noreply.github.com>
2019-08-26 23:34:07 +0530

    Add files via upload

--- Cache.java
+++ Cache.java
@@ -1,4 +1,15 @@
 package com.springboot.project.searchengine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cache {
+
+    public static void main(String[] args){
+        Map<Integer, String> hashmap = new HashMap<>(10,0.75F);
+
+        User user1 = new User();
+    }
+
+
 }
